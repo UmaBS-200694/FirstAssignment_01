566e2319db59dc7dfe1e64ff1397534ba832f91d
UmaBS-200694 <umabs622@gmail.com>
2025-03-04 19:49:51 +0530

    New code change

--- src/newDevOps_03/newDevOps_03.java
+++ src/newDevOps_03/newDevOps_03.java
@@ -5,8 +5,14 @@ public class newDevOps_03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 M1();
+M2();//Added method 2 in code change
 	}
 	public static void M1() {
+		
+		
+		System.out.println("First Assgnment" );
+	}
+public static void M2() {
 		
 		
 		System.out.println("First Assgnment" );
