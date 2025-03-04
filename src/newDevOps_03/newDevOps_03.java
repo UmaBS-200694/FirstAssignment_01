b63a98e0747af4982c20836c9a1e06386f8c75c1
UmaBS-200694 <umabs622@gmail.com>
2025-03-04 19:47:25 +0530

    First Assignment

---
+++ src/newDevOps_03/newDevOps_03.java
@@ -0,0 +1,14 @@
+package newDevOps_03;
+
+public class newDevOps_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+M1();
+	}
+	public static void M1() {
+		
+		
+		System.out.println("First Assgnment" );
+	}
+}
